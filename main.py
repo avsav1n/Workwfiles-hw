c1aece0786367c80c5d3c6e81a8d3861df52fa4b
Aleksei Savin <avsavin93@mail.ru>
2024-05-04 12:59:01 +0300

    All tasks resolved

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from glob import glob
 import os
 
 def count_ingredients(persons: int, *dishes: tuple) -> dict:
@@ -45,20 +46,40 @@ def create_cook_book(external_file: str) -> dict:
                 number_of_lines -= 1
     return cook_book
 
-def union_files(*files):
-    with open(r'union\solution.txt', 'w', encoding='utf-8') as l:
-        for i in files:
-            with open(i, 'r', encoding='utf-8') as p:
-                l.write(p.readline())
+def union_files(direct):
+    '''
+    Задача №3. Функция объединения файлов в принимаемой директории с учетом 
+    сортировки файлов по возрастанию количества в них строк
+    '''
+    if os.path.exists(direct):
+        lines_list = []
+        source_files = glob(os.path.join(direct, '*.txt'))
+        source_files = [file for file in source_files 
+                        if file != 'union\\solution.txt']    
+    for file in source_files:
+        file_name = file.split('\\')[1]
+        with open(file, encoding='utf-8') as fr:
+            lines_list.append((fr.readlines(), file_name))
+    lines_list = sorted(lines_list, key=lambda x: len(x[0]))
+    with open(r'union\solution.txt', 'w', encoding='utf-8') as fw:
+        for lines, file_name in lines_list:
+            fw.write(f'{file_name}\n{len(lines)}\n')
+            line = [f'Строка №{pos+1} файла №{file_name[0]} {line}' 
+                    for pos, line in enumerate(lines)]
+            line.append('\n')
+            fw.write(''.join(line))
 
 
 
-# cook_book = create_cook_book('recipes.txt')
-# print(f'\nСформированная из файла кулинарная книга:\n{"*"*41}')
-# pprint(cook_book, width=100)
+cook_book = create_cook_book('recipes.txt')
+print(f'\nСформированная из файла кулинарная книга:\n{"*"*41}')
+pprint(cook_book, width=100)
 
-# list_for_shopping = count_ingredients(3, 'Утка по-пекински', 'Омлет', 'Гречка', 'Гороховый суп')
-# print(f'Итоговый список ингредиентов, которые нужно купить:\n{"*"*51}')
-# pprint(list_for_shopping)
+list_for_shopping = count_ingredients(3, 'Утка по-пекински', 'Омлет', 'Гречка', 'Гороховый суп')
+print(f'Итоговый список ингредиентов, которые нужно купить:\n{"*"*51}')
+pprint(list_for_shopping)
 
-union_files(r'union\1.txt', r'union\2.txt', r'union\3.txt')
+union_files('union')
+print(f'\nСформированный и отсортированный документ, ' 
+      f'собранный из файлов, располагаемых в директории \\union, '
+      f'доступен по ссылке union\\solution.txt\n')
